bdf9da8f2535656cbc704abecb413165d38f0090
“Revaz <generationofjeans@gmail.com>
2025-06-17 23:16:24 +0400

    add fields for customer registration function git status '' '

--- src/main/java/com/example/customer_notification_system/entity/Customer.java
+++ src/main/java/com/example/customer_notification_system/entity/Customer.java
@@ -1,7 +1,9 @@
 package com.example.customer_notification_system.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -16,8 +18,16 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false) // New: Username for login, must be unique and not null
+    private String username;
+
+    @Column(nullable = false) // New: Hashed password for login, must not be null
+    private String password;
+
+    @Column(nullable = false)
     private String fullName;
 
+    @Column(unique = true) // Email should ideally be unique for customer records
     private String email;
 
     private String phoneNumber;
