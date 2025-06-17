2e02faaaf4be78313eb1e75d56c77e09f800f650
“Revaz <generationofjeans@gmail.com>
2025-06-17 23:22:25 +0400

    add registration request DTO file

---
+++ src/main/java/com/example/customer_notification_system/dto/requests/RegisterUserRequest.java
@@ -0,0 +1,32 @@
+package com.example.customer_notification_system.dto.requests;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterUserRequest {
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
+
+    @NotBlank(message = "Full name cannot be blank")
+    private String fullName;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @Pattern(regexp = "^[0-9+\\- ]{8,15}$", message = "Phone number must be between 8 and 15 digits/symbols")
+    private String phoneNumber;
+}
