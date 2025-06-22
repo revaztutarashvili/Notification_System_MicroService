6285c684943581b667a2a235e06fbe2c275ba426
“Revaz <generationofjeans@gmail.com>
2025-06-22 04:40:44 +0400

    refactor strctr

--- src/main/java/com/example/customer_notification_system/CustomerNotificationSystemApplication.java
+++ src/main/java/com/example/customer_notification_system/CustomerNotificationSystemApplication.java
@@ -9,5 +9,5 @@ public class CustomerNotificationSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerNotificationSystemApplication.class, args);
 	}
-
+ 
 }
